7eb0ee40e082f4c7695605ed5cb0eebbf769613a
Gustavoctt <gustavocarrertartare@gmail.com>
2022-07-07 18:20:19 -0300

    Create Single Post Page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,11 +2,13 @@ import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Header from '../../components/Header';
+import { FiCalendar, FiUser, FiClock  } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -30,14 +32,44 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
-  console.log(post)
-
   return(
     <>
+      <head>
+        <title>{post.data.title} | SpaceTraveling</title>
+      </head>
       <Header/>
 
       <article className={styles.container}>
         <img src={post.data.banner.url} alt="banner post" />
+
+        <div className={styles.containerPost}>
+          <h1>Criando um app CRA do zero</h1>
+          <div className={styles.informationPost}>
+            <time> 
+              <FiCalendar size={20} />
+              15 Mar 2021
+            </time>
+            <span> 
+              <FiUser size={20}/>
+              15 Mar 2021
+            </span>
+            <span> 
+              <FiClock size={20} />
+              03 min
+              </span>
+          </div>
+
+          <div className={styles.content}>
+           {post.data.content.map(content => (
+             <div key={content.heading} >
+               <h2>{ content.heading }</h2>
+               <div className={styles.contentBody}>
+                 <p>{RichText.asText(content.body)}</p>
+               </div>
+             </div>
+           ))}
+          </div>
+        </div>
       </article>
     </>
   )
